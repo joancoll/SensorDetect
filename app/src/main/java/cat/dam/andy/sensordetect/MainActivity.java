9eb8b95a25ce2d1dc5c88266776e172959285745
joancoll <joancoll@inspladelestany.cat>
2023-02-03 10:12:37 +0100

    create main methods

--- app/src/main/java/cat/dam/andy/sensordetect/MainActivity.java
+++ app/src/main/java/cat/dam/andy/sensordetect/MainActivity.java
@@ -1,10 +1,13 @@
 package cat.dam.andy.sensordetect;
+
 import android.content.Context;
-import android.hardware.SensorManager;
 import android.hardware.Sensor;
-import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.List;
 
 /**
@@ -12,25 +15,35 @@ import java.util.List;
  * de sensors disponibles, i mostra la llista com a TextView.
  */
 public class MainActivity extends AppCompatActivity {
-
-    // Crea una instància SensorManager per accedir als sensors del sistema.
-    private SensorManager mSensorManager;
+    // Members
+    private TextView tv_sensors;
+    // instància SensorManager per accedir als sensors del sistema.
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        showSensorList();
+    }
 
-        TextView tvSensors = (TextView) findViewById(R.id.tv_Sensors);
+
+
+    private void initViews() {
+        tv_sensors = findViewById(R.id.tv_sensors);
+    }
+
+    private void showSensorList() {
         int nSensors=0; //quantitat de sensors
 
         //getSystemService retorna un handler (gestor) d'un servei a nivell de sistema pel seu nom
         //amb getSystemService podem accedir no només als sensors, també al gestor de bateria, vibració, alarma, ....
         //En aquest cas el handler serà del tipus SensorManager i connectarà amb el servei de sensors
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         //cridem el mètode .getSensorList que obté la llista de tots els sensors disponibles (tipus Sensor)
         //podriem triar de quin tipus volem comprovar p.e. TYPE_AMBIENT_TEMPERATURE
-        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
         //Itera la llista de sensors, obté nom del sensor i l'afegeix a un string amb un salt de línia.
         //Utilitzem StringBuilder enlloc de concat o + per rendiment ja que el compilador crearà menys objectes
@@ -40,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
             textSensors.append(nSensors).append("-").append(currentSensor.getName()).append(
                     System.getProperty("line.separator")); //No depèn SO (\n Linux o \r\n Windows)
         }
-
         // Actualitza el textview amb la llista de sensors disponibles del dispositiu.
-        tvSensors.setText(textSensors);
+        tv_sensors.setText(textSensors);
     }
 }
